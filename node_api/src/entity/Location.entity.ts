8c5127c699a760c87a0977b51c02bbd9253499e2
Vladislav-UZH <perebziak.vladislav@gmail.com>
2025-12-31 17:12:17 +0200

    add repos

--- node_api/src/entity/Location.entity.ts
+++ node_api/src/entity/Location.entity.ts
@@ -1,4 +1,4 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type LocationDocument = HydratedDocument<Location>;
@@ -8,11 +8,15 @@ export class Location {
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
-  lat: number;
-
-  @Prop({ required: true })
-  lng: number;
+  @Prop(
+    raw({
+      type: { type: String, enum: ['Point'], default: 'Point', required: true },
+      coordinates: { type: [Number], required: true },
+    }),
+  )
+  location: { type: 'Point'; coordinates: [number, number] };
 }
 
 export const LocationSchema = SchemaFactory.createForClass(Location);
+
+LocationSchema.index({ location: '2dsphere' });
