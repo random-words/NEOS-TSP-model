92c6667961cbfcf889af2c7f1410ab38853260f5
Antonio <sivko.anton.ua@gmail.com>
2026-02-01 23:06:54 +0200

    create request/response data for distance/time/budget; update some fields in contracts

--- engine_api/math_service/math_model/tsp_config.py
+++ engine_api/math_service/math_model/tsp_config.py
@@ -12,17 +12,17 @@ NEOS_EMAIL = "antonsivko480@gmail.com"
 # CONFIG: TSP / k-cycle
 # ----------------------------
 s_value = 1
-k_value = 8
+k_value = 5
 
 # ----------------------------
 # CONFIG: travel/time/budget parameters
 # ----------------------------
 group_size = 2
 speed_kmph = 50.0
-budget_max = 100000
-time_max   = 24 * 360.0 # In Hours
+budget_max = 35000
+time_max   = 24 * 60.0 # In Hours (because in default its minutes)
 dist_max = None # If Needed
-route_pace = 90
+route_pace = 90 # In Minutes
 
 CONNECTIVITY = "hybrid" # "flow" | "mtz" | "hybrid"
 
